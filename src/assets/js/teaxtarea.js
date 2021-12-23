8db26ca5701ed17fac19c87b1f1a3a52f3d3e75c
Zhilenko Matvey <mateyzhilenko@gmail.com>
2021-12-23 22:36:49 +0300

    personal-blog
    personal-blog

---
+++ src/assets/js/teaxtarea.js
@@ -0,0 +1,12 @@
+const textarea = document.querySelectorAll('[data-autoresize]');
+
+textarea.forEach(item => {
+    let textareaH = item.offsetHeight;
+
+    item.addEventListener('input', event => {
+        $this = event.target;
+
+        $this.style.height = textareaH + 'px';
+        $this.style.height = $this.scrollHeight + 'px';
+    });
+});
